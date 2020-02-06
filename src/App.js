161e5accdae30da4c55d7c4a66da8dc93d1b02e5
Gilson Cavalcanti <gilsoncav@gmail.com>
2020-02-06 15:20:10 -0300

    configured access to AWS services via amplify

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import "./App.css";
+import Routes from "./Routes";
+import {LinkContainer} from "react-router-bootstrap"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    return (
+        <div className="App container">
+            <Navbar fluid collapseOnSelect>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <Link to="/">Scratch</Link>
+                     </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav pullRight>
+                        <LinkContainer to={"/signup"}>
+                            <NavItem href="/signup">Signup </NavItem>
+                        </LinkContainer>
+                        <LinkContainer to={"/login"}>
+                            <NavItem href="/login">Login</NavItem>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <Routes />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "sa-east-1",
+        BUCKET: "gilsoncav-notes-app-uploads"
+    },
+    apiGateway: {
+        REGION: "sa-east-1",
+        URL: "https://q4phlr5297.execute-api.sa-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_m3QIEZrUk",
+        APP_CLIENT_ID: "q6vh8deg37hbhn81tec0pf8vq",
+        IDENTITY_POOL_ID: "us-east-2:c486bf5a-869b-47c6-a955-65c74253651b"
+    }
+};
